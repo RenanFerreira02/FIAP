4761c68b521ac1cbe08d723f2725be41323a079b
RenanFerreira02 <renankferreira01@gmail.com>
2024-03-07 20:51:07 -0300

    Primeiro exercicio de Java

---
+++ 1SIR/Lab de Programação/Exercicios_01/src/exercicio1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class exercicio1 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		double base, altura, area, perimetro;
+		
+		System.out.println("Calcule a Área e o Perímetro de um retângulo\n");
+		
+		System.out.print("Digite o valor da base (em cm) ");
+		base = input.nextDouble();
+		
+		System.out.print("Digite o valor da altura (em cm) ");
+		altura = input.nextDouble();
+		
+		area = base * altura;
+		
+		perimetro = (base + altura) * 2;
+		
+		System.out.println("A área é " + area + " e o perímetro é " + perimetro);
+		
+		
+		
+
+	}
+
+}
