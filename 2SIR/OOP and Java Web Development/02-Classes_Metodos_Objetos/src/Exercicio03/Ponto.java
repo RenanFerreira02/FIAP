39cb93e8a2938e814d6f144ce4414532c7587264
RenanFerreira02 <renankferreira01@gmail.com>
2025-03-06 19:39:03 -0300

    Exercícios POO

---
+++ 2SIR/OOP and Java Web Development/02-Classes_Metodos_Objetos/src/Exercicio03/Ponto.java
@@ -0,0 +1,40 @@
+package Exercicio03;
+
+public class Ponto {
+    // Variáveis de instância ou Atributos
+    double x;
+    double y;
+
+
+    // Método para calcular e retornar a distância entre dois pontos
+    public double calcularDistancia(Ponto p) {
+        double distancia = Math.sqrt(Math.pow(p.x - x, 2) + Math.pow(p.y - y, 2));
+
+        return distancia;
+    }
+
+    // Método para calcular e retornar a distância de um ponto até a origem
+    public double calcularDistanciaAteOrigem() {
+        Ponto origem = new Ponto();
+
+        return calcularDistancia(origem);
+    }
+
+    // Método para calcular e retornar o ponto mais proximo da origem
+    public Ponto maisProximoDaOrigem(Ponto p1, Ponto p2) {
+        double dp1 = p1.calcularDistanciaAteOrigem();
+        double dp2 = p2.calcularDistanciaAteOrigem();
+
+        if (dp1 < dp2) {
+            return p1;
+        }
+        return p2;
+    }
+
+    // Método para retornar os dados do objeto formatado
+    public String formatarPonto() {
+        return "(" + x + "," + y + ")";
+    }
+
+
+}
