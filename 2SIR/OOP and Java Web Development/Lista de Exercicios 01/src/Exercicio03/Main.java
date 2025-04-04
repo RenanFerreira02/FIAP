cef61881d4e62c8374af84dafdbd7c0beb29b84b
RenanFerreira02 <renankferreira01@gmail.com>
2025-04-03 22:38:34 -0300

    Exercício 3

---
+++ 2SIR/OOP and Java Web Development/Lista de Exercicios 01/src/Exercicio03/Main.java
@@ -0,0 +1,10 @@
+package Exercicio03;
+
+public class Main {
+    public static void main(String[] args) {
+        Retangulo r = new Retangulo(2,8);
+
+        System.out.println(r.calcualarArea());
+        System.out.println(r.calcualarPerimetro());
+    }
+}
---
+++ 2SIR/OOP and Java Web Development/Lista de Exercicios 01/src/Exercicio03/Retangulo.java
@@ -0,0 +1,35 @@
+package Exercicio03;
+
+public class Retangulo {
+    private double base;
+    private double altura;
+
+    public Retangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double calcualarArea() {
+        return (base * altura);
+    }
+
+    public double calcualarPerimetro() {
+        return 2 * (base + altura);
+    }
+}
