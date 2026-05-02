a53b0a7fac03652ce1b83ca3fcf5fde094195680
RenanFerreira02 <renankferreira01@gmail.com>
2026-05-02 11:25:52 -0300

    Organização do repositório

--- 3SION/Fase 3/Omni-Tribo/Omni-Tribo/app/src/main/java/br/com/fiap/omni_tribo/utils/Formatters.kt
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.omni_tribo.utils
-
-fun Int.toFormattedXp(): String =
-    toString().reversed().chunked(3).joinToString(".").reversed()
-
-fun Double.toBrlFormatted(): String =
-    "R$ ${"%.2f".format(this).replace(".", ",")}"
